f60a7fca98c56823707d00d132ebb27f312e51be
Corentin <mikescops@pixelswap.fr>
2018-11-01 10:22:37 +0100

    Add pagination to list

--- list.php
+++ list.php
@@ -55,6 +55,22 @@
           </div>
         </div>
       </div>
+
+      <hr>
+
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-end">
+          <li class="page-item disabled">
+            <a class="page-link" href="#" tabindex="-1">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 
